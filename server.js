76dab3aff67daf5b5617baf3141f29ca6f60ad34
Taylor Harwin <tharwin@plaid.com>
2016-07-28 18:21:06 -0700

    stuck on authenticate route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var plaid = require('plaid');
 
 var port = process.env.PORT || 1337;
 process.env.NODE_ENV = 'production';
@@ -6,9 +7,16 @@ var app = express();
 app.use(express.static(__dirname));
 
 
-    app.get('/', function(req, res){
-        res.render('index');
-    });
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.post('/accounts', function(req, res){
+	console.log('got a response')
+	var public_token = req.body.public_token;
+	console.log(public_token);
+});
+
 
 app.listen(port);
 console.log('Listening on port' + port);
