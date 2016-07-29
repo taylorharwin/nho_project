5eb2bcec2283cda2afc84d249da3154acf3cf1bc
Taylor Harwin <tharwin@plaid.com>
2016-07-28 17:42:49 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var port = process.env.PORT || 1337;
+process.env.NODE_ENV = 'production';
+var app = express();
+app.use(express.static(__dirname));
+
+
+    app.get('/', function(req, res){
+        res.render('index');
+    });
+
+app.listen(port);
+console.log('Listening on port' + port);
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entry: './app.jsx',
+    output: {
+    },
+    module: {
+        loaders: [
+            {
+                //tell webpack to use jsx-loader for all *.jsx files
+                test: /\.jsx$/,
+                loader: 'jsx-loader?insertPragma=React.DOM&harmony'
+            }
+        ]
+    },
+    externals: {
+        //don't bundle the 'react' npm package with our bundle.js
+        //but get it from a global 'React' variable
+        'react': 'React'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+}
