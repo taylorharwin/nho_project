8c2107352ef97cfad89194c29c7ff8d8b0a48e41
Taylor Harwin <tharwin@plaid.com>
2016-07-30 00:38:39 -0700

    trapezoid

--- pyramid.jsx
+++ pyramid.jsx
@@ -1,4 +1,5 @@
 var react = require('react');
+var reactDom = require('react-dom');
 var _ = require('lodash');
 var needsMap = require('./needsMap.js');
 
@@ -6,24 +7,25 @@ var Pyramid = react.createClass({
 	getRows: function(pyramid){
 		var total = _.reduce(pyramid, function(accum, val){return accum + val}, 0);
 		var rows = _.map(pyramid, function(val, key){
-			var average = Math.round((val / total * 100) * 100) / 100 + '%';
+			var average = (val / total);
+			var displayAverage = (average * 100).toFixed(2);
+			var pxHeight = (average * 300) + 'px';
+
 
 			var name = needsMap[key].display,
 				rank = needsMap[key].rank;
 			var style = {
-				borderRight: '1px solid black',
 				borderBottom: '1px solid black',
-				borderLeft: '1px solid black',
-				height: average,
-				minHeight:'14px'
+				height: pxHeight
 			}
-
 			var trapWidth = (rank * 60) + 'px';
 			return (
-				<div className="table" rel={rank} style={style}> 
+				<div className="table" rel={rank} style={style}>
 					<div className="table_cell">
-						<span className="table_text">{name} : {average}% </span>
+						<span className="table_text">{name} : {displayAverage}% </span>
 					</div>
+										<div style={{height: pxHeight, width: trapWidth}}className="trapezoid"></div> 
+
 				</div>)
 		});
 
